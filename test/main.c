63bc61afe43fe13b2ff5f4478a0733602d6f9a0d
danilo_d <danilo_d@epitech.eu>
2015-05-23 21:31:37 +0200

    test_unitaire

---
+++ test/main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for main in /home/danilo_d/semestre2/PSU_2014_42sh/test
+**
+** Made by danilov dimitri
+** Login   <danilo_d@epitech.eu>
+**
+** Started on  Sat May 23 21:27:29 2015 danilov dimitri
+** Last update Sat May 23 21:27:43 2015 danilov dimitri
+*/
+
+int		main(int ac, char **av)
+{
+  char		*str;
+
+  str[100] = 1;
+
+}
