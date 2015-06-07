aedf336dc2f43c73dfc6f420f6391ea15673acde
boulay_b <arnaud.boulay@epitech.eu>
2015-06-07 17:03:42 +0200

    ambiguous_reborn

--- sources/syntax.c
+++ sources/syntax.c
@@ -5,7 +5,7 @@
 ** Login   <boulay_b@epitech.net>
 **
 ** Started on  Wed May 20 16:04:45 2015 Arnaud Boulay
-** Last update Fri Jun  5 18:00:04 2015 Dylan Coodien
+** Last update Sun Jun  7 17:03:13 2015 Arnaud Boulay
 */
 
 #include <stdio.h>
@@ -27,27 +27,26 @@ static int	is_op(char *str)
     return (0);
 }
 
-static int	check_ambigious_out(int *redir_right, char **line,
-				    int i, t_env *env_list)
+static int	check_ambigious_out(int *redir_right, char *str, char *last)
 {
-  if (strcmp(line[i], ">>") == 0 || strcmp(line[i], ">") == 0)
+  if (strcmp(str, ">>") == 0 || strcmp(str, ">") == 0 ||
+      (last != NULL && (strcmp(last, ">>") == 0 || strcmp(last, ">") == 0)
+       && strcmp(str, "|") == 0))
     {
-      if (*redir_right == 1)
+      if (*redir_right == 1 || strcmp(str, "|") == 0)
 	{
 	  printf("Ambiguous output redirect.\n");
-	  disp_prompt(env_list);
-	  free_tab(line);
 	  return (1);
 	}
       else
 	*redir_right = 1;
     }
-  else if (strcmp(line[i], ";") == 0 && *redir_right == 1)
+  else if (strcmp(str, ";") == 0 && *redir_right == 1)
     *redir_right = 0;
   return (0);
 }
 
-static int	check_syntax_core(char **line, int i, t_env *env_list)
+static int	check_syntax_core(char **line, int i)
 {
   if ((i == 0 && is_op(line[i]) == 1) ||
       (i != 0 && is_op(line[i - 1]) == 1 && is_op(line[i]) == 1) ||
@@ -56,35 +55,31 @@ static int	check_syntax_core(char **line, int i, t_env *env_list)
       (is_op(line[i]) == 1 && is_op(line[i + 1]) == 1))
     {
       printf("Invalid null command.\n");
-      disp_prompt(env_list);
-      free_tab(line);
       return (1);
     }
   return (0);
 }
 
-static int	check_ambigious_in(int *redir_left, char **line,
-				   int i, t_env *env_list)
+static int	check_ambigious_in(int *redir_left, char *str, char *last)
 {
-  if (strcmp(line[i], "<<") == 0 || strcmp(line[i], "<") == 0)
+  if (strcmp(str, "<<") == 0 || strcmp(str, "<") == 0)
     {
-      if (*redir_left == 1)
+      if (*redir_left == 1 || (last != NULL && strcmp(last, "|") == 0))
 	{
 	  printf("Ambiguous input redirect.\n");
-	  disp_prompt(env_list);
-	  free_tab(line);
 	  return (1);
 	}
       else
 	*redir_left = 1;
     }
-  else if (strcmp(line[i], ";") == 0 && *redir_left == 1)
+  else if (strcmp(str, ";") == 0 && *redir_left == 1)
     *redir_left = 0;
   return (0);
 }
 
 int		check_syntax(char **line, t_env *env_list)
 {
+  char		*last;
   int		redir_right;
   int		redir_left;
   int		i;
@@ -92,14 +87,20 @@ int		check_syntax(char **line, t_env *env_list)
   i = -1;
   redir_right = 0;
   redir_left = 0;
+  last = NULL;
   while (line[++i] != NULL)
-    if (check_syntax_core(line, i, env_list) == 1 ||
-	check_ambigious_in(&redir_left, line, i, env_list) == 1 ||
-	check_ambigious_out(&redir_right, line, i, env_list) == 1)
-      {
-	check_status(g_env, 1);
-	return (-1);
-      }
+    {
+      if (check_syntax_core(line, i) == 1 ||
+	  check_ambigious_in(&redir_left, line[i], last) == 1 ||
+	  check_ambigious_out(&redir_right, line[i], last) == 1)
+	{
+	  disp_prompt(env_list);
+	  free_tab(line);
+	  return (-1);
+	}
+      if (is_op(line[i]) == 1 || strcmp(line[i], ";") == 0)
+	last = line[i];
+    }
   free_tab(line);
   return (0);
 }
